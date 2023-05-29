844b340f0cdf275f62fe4332f93b1c3a3250de72
martinrodfl <martinrodf@gmail.com>
2023-05-28 22:15:48 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,195 @@
+//* 1. How to Capitalize Text -------------------------------------------  */
+function CapitalizeWord() {
+  let inputValueText = document.getElementById('inputText').value;
+  let capitalizedWord =
+    inputValueText.charAt(0).toUpperCase() + inputValueText.slice(1);
+
+  document.getElementById('resultText').innerHTML = capitalizedWord;
+}
+
+//* 2. How to Calculate Percent ------------------------------------------- */
+function CalculatePercent() {
+  let inputPercentageNumber = document.getElementById(
+    'inputPercentageNumber'
+  ).value;
+  let inputValueNumber = document.getElementById('inputValueNumber').value;
+
+  const calculate = (inputValueNumber, inputPercentageNumber) => {
+    return (inputValueNumber * inputPercentageNumber) / 100;
+  };
+
+  document.getElementById('resultPercentage').innerHTML = calculate(
+    inputPercentageNumber,
+    inputValueNumber
+  );
+}
+//*  3. How to Get a Random Element ------------------------------------------- */
+var items = ['Martin', 'Maria', 'Juan', 'Andrea'];
+
+function HandleCheckboxChange(checkboxId) {
+  let checkboxes = document.getElementsByName('group');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.checked = checkbox.id === checkboxId;
+  });
+
+  if (checkboxId === 'Names') {
+    items = ['Martin', 'Maria', 'Juan', 'Andrea'];
+  } else if (checkboxId === 'Numbers') {
+    items = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  } else if (checkboxId === 'Colors') {
+    items = ['Blue', 'White', 'Red', 'Green', 'Gray', 'Pink'];
+  }
+
+  showSelectedCheckboxId();
+  return checkboxId;
+}
+
+function showSelectedCheckboxId() {
+  let checkboxes = document.getElementsByName('group');
+  let selectedCheckboxId = '';
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      selectedCheckboxId = checkbox.id;
+    }
+  });
+  document.getElementById('selectedCheckboxId').innerHTML = selectedCheckboxId;
+}
+
+function randomItem() {
+  if (items === '') {
+    alert('You must select a checkbox');
+  }
+  const getRandomItem = items[Math.floor(Math.random() * items.length)];
+  document.getElementById('resultRandom').innerHTML = getRandomItem;
+}
+showSelectedCheckboxId();
+
+//* 4. How to Remove Duplicate Elements -------------------------------------------  */
+let itemsDuplicates = ['Martin', 'Martin', 'Maria', 'Juan', 'Andrea', 'Andrea'];
+var selectedCheckboxId = '';
+function HandleCheckboxRepeat(checkboxId) {
+  const checkboxes = document.getElementsByName('group2');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.checked = checkbox.id === checkboxId;
+  });
+  if (checkboxId === 'Names-Duplicates') {
+    itemsDuplicates = ['Martin', 'Martin', 'Maria', 'Juan', 'Andrea', 'Andrea'];
+  } else if (checkboxId === 'Numbers-Duplicates') {
+    itemsDuplicates = [
+      '0',
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '5',
+      '6',
+      '7',
+      '8',
+      '8',
+      '9',
+    ];
+  } else if (checkboxId === 'Colors-Duplicates') {
+    itemsDuplicates = [
+      'Blue',
+      'Blue',
+      'White',
+      'Red',
+      'Green',
+      'Green',
+      'Gray',
+      'Pink',
+      'Pink',
+    ];
+  }
+  showSelectedCheckboxIdRepeat();
+  return checkboxId;
+}
+
+function showSelectedCheckboxIdRepeat() {
+  const checkboxes = document.getElementsByName('group2');
+  selectedCheckboxId = '';
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      selectedCheckboxId = checkbox.id;
+    }
+    console.log(selectedCheckboxId);
+  });
+  document.getElementById('selectedCheckboxIdDuplicates').innerHTML =
+    selectedCheckboxId;
+}
+
+let initName = selectedCheckboxId;
+
+function NoRepeatItems() {
+  if (itemsDuplicates === '') {
+    alert('You must select a checkbox');
+    return;
+  }
+
+  function removeAfterHyphen(string) {
+    var hyphenIndex = string.indexOf('-');
+    if (hyphenIndex !== -1) {
+      string = string.slice(0, hyphenIndex);
+    }
+    return string;
+  }
+
+  const removeDuplicates = (items) => [...new Set(items)];
+  document.getElementById('result-Duplicates').innerHTML =
+    removeAfterHyphen(selectedCheckboxId) +
+    ': ' +
+    removeDuplicates(itemsDuplicates);
+}
+
+showSelectedCheckboxIdRepeat();
+
+//* 5. How to Sort Elements By Certain Property -------------------------------------------  */
+let lessons = [
+  { position: 0, name: 'Intro', duration: 18 },
+  { position: 1, name: 'Basics', duration: 12 },
+  { position: 2, name: 'Advanced', duration: 15 },
+];
+
+let selectedRadioId = 'position';
+
+function HandleRadioChecked() {
+  const radios = document.getElementsByName('group3');
+  radios.forEach(function (radio) {
+    if (radio.checked) {
+      selectedRadioId = radio.id;
+    }
+  });
+
+  document.getElementById('selectedRadioSortedId').innerHTML = selectedRadioId;
+}
+
+function SortLessons() {
+  if (selectedRadioId === '') {
+    alert('You must select a radio button');
+    return;
+  }
+
+  let sortedLessons = lessons.sort((a, b) =>
+    a[selectedRadioId] > b[selectedRadioId]
+      ? 1
+      : a[selectedRadioId] < b[selectedRadioId]
+      ? -1
+      : 0
+  );
+
+  let result = document.getElementById('result-Sort');
+  result.innerHTML = '';
+
+  let ul = document.createElement('ul');
+  sortedLessons.forEach(function (lesson) {
+    let li = document.createElement('li');
+    li.textContent = JSON.stringify(lesson);
+    ul.appendChild(li);
+  });
+
+  result.appendChild(ul);
+}
+
+HandleRadioChecked();
